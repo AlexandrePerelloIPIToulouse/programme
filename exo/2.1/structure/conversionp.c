769bc44bb3b04fbc29e0d757719ec62af4ca071f
ALex <alexandre.perello1@campus-igs-toulouse.fr>
2022-02-01 19:51:41 +0100

    v2.1

--- exo/2.1/structure/conversionp.c
+++ exo/2.1/structure/conversionp.c
@@ -6,9 +6,9 @@ float ps1=14.5038;
 	
 	printf("BAR\tPSI\n");
 	for (float i=1 ; i <9 ; i=i+0.2){
-	bar=i*psi;
+	bar=i*ps1;
 
-	printf("%.1f\t %.1f\n", bar,i);
+	printf("%.1f\t%.1f\n", bar,i);
 	}
 
 
