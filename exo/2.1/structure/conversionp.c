0e15c3d00a0ffb6f4fb8e477d19e3c84cc4ff37d
Perello <97890284+AlexandrePerelloIPIToulouse@users.noreply.github.com>
2022-02-01 17:20:25 +0100

    Update conversionp.c

--- exo/2.1/structure/conversionp.c
+++ exo/2.1/structure/conversionp.c
@@ -2,9 +2,11 @@
 
 int main(){
 float bar =0;
+float ps1=14.5038;
+	
 	printf("BAR\tPSI\n");
 	for (float i=1 ; i <9 ; i=i+0.2){
-	bar=i*14.5038;
+	bar=i*psi;
 
 	printf("%.1f\t %.1f\n", bar,i);
 	}
